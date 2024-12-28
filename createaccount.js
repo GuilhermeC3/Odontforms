ce0a3d2bc6ba619ed14878a506a72e95c794e29d
GuilhermeC3Dev <guilhermecosta030409@gmail.com>
2024-12-28 14:45:18 -0300

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -1,15 +1,17 @@
-document.getElementById('createAccountButton').addEventListener('click', function() {
+document.getElementById('createAccountForm').addEventListener('submit', function(event) {
     var nome = document.getElementById('nome').value;
     var email = document.getElementById('email').value;
     var senha = document.getElementById('senha').value;
     var confirmSenha = document.getElementById('confirmsenha').value;
     var errorMessage = document.getElementById('error-message');
 
+    errorMessage.textContent = ''; 
+
     if (nome === '' || email === '' || senha === '' || confirmSenha === '') {
         errorMessage.textContent = 'Preencha todos os campos.';
+        event.preventDefault(); 
     } else if (senha !== confirmSenha) {
         errorMessage.textContent = 'As senhas não coincidem.';
-    } else {
-        document.getElementById('createAccountForm').submit();
+        event.preventDefault(); 
     }
 });
